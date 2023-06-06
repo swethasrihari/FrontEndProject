a11621aef51e61349db5ecdbeaf8bb109cd3c4a7
ssiddhi <ssiddhi@pdx.edu>
2023-06-06 08:02:17 -0700

    modified fuel table, fixed warnings and added Fuelname

---
+++ FuelTableUpdate/src/components/FuelData.js
@@ -0,0 +1,24 @@
+const FuelData = ({ fuel }) => {
+  const fuelTT = fuel.filter((item) => {
+    return item.sectorId === "TT";
+  });
+  return (
+    <>
+      {""}
+      {fuelTT.map((curFuel) => {
+        const { period, "fuel-name": fuelName, value } = curFuel;
+
+        const uniqueKey = `${period}${fuelName}`;
+
+        return (
+          <tr key={uniqueKey}>
+            <td>{period}</td>
+            <td>{fuelName}</td>
+            <td>{value}</td>
+          </tr>
+        );
+      })}
+    </>
+  );
+};
+export default FuelData;
