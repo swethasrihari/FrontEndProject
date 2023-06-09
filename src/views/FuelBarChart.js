fd875a26836b17d9df959cb0ccf089daef55057f
ssiddhi <108196168+ssiddhi@users.noreply.github.com>
2023-06-09 15:09:06 -0700

    Update FuelBarChart.js
    Modified bordercolors and title

--- src/views/FuelBarChart.js
+++ src/views/FuelBarChart.js
@@ -1,97 +1,14 @@
-// import "chart.js/auto";
-// import { useEffect, useState } from "react";
-// import { Bar } from "react-chartjs-2";
-// import { backgroundColors, borderColors } from "../utils/chartColors";
-
-// const BarChart = () => {
-//   const [chart, setChart] = useState([]);
-//   const [errorMessage, setErrorMessage] = useState("");
-
-//   const baseUrl =
-//     "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?api_key=29TYpqceK8M5v5YN9wc919BanZ4LMogwwDKwJIXL&facets[stateId][]=US&data[]=value";
-
-//   useEffect(() => {
-//     const fetchCO2Emissions = async () => {
-//       await fetch(`${baseUrl}`, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application-json"
-//         }
-//       })
-//         .then((response) => {
-//           response.json().then((json) => {
-//             //console.log(json);
-//             setChart(json.response.data);
-//           });
-//         })
-//         .catch((error) => {
-//           setErrorMessage(error);
-//           console.log(errorMessage);
-//         });
-//     };
-//     fetchCO2Emissions();
-//   }, [baseUrl, errorMessage]);
-
-//   let amount = [];
-//   let year = [];
-//   chart.forEach((item) => {
-//     if (
-//       item.period >= 2015 &&
-//       item.period <= 2020 &&
-//       item.fuelId === "TO" &&
-//       item.sectorId === "TT"
-//     ) {
-//       amount.push(item.value);
-//       year.push(item.period);
-//     }
-//   });
-
-//   const data = {
-//     labels: year,
-
-//     datasets: [
-//       {
-//         label: `CO2 Emissions from Fuel`,
-
-//         data: amount,
-
-//         backgroundColor: backgroundColors,
-//         borderColor: borderColors,
-
-//         borderWidth: 1
-//       }
-//     ]
-//   };
-//   return (
-//     <div className="barchart">
-//       <div className="card m-4">
-//         {" "}
-//         <div className="card-body">
-//           <div className="card-title">
-//             <h2>Last 5 years CO2 emissions</h2>
-
-//             <Bar data={data} options={{ responsive: true, aspectRatio: 1.7 }} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-// export default BarChart;
-import "chart.js/auto";
 import React, { useEffect, useState, useContext } from "react";
 import { Bar } from "react-chartjs-2";
 import { ModeContext } from "../App";
-import { backgroundColors, borderColors } from "../utils/chartColors";
 
 const BarChart = () => {
   const mode = useContext(ModeContext);
-
   const [chart, setChart] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
   const baseUrl =
-    "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?api_key=29TYpqceK8M5v5YN9wc919BanZ4LMogwwDKwJIXL&facets[stateId][]=US&data[]=value";
+    "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?api_key=29TYpqceK8M5v5YN9wc919BanZ4LMogwwDKwJIXL&data[]=value&facets[fuelId][]=TO&facets[sectorId][]=CC&facets[sectorId][]=EC&facets[sectorId][]=IC&facets[sectorId][]=RC&facets[sectorId][]=TC&facets[stateId][]=OR";
 
   useEffect(() => {
     const fetchCO2Emissions = async () => {
@@ -116,67 +33,125 @@ const BarChart = () => {
   let amount = [];
   let year = [];
   let sortYear = [];
+  let sectorArray = [];
+  let startYear = 2016;
+  let endYear = 2020;
+  let yearArr = endYear - startYear + 1;
+
   chart.forEach((item) => {
-    if (item.period >= 2015 && item.period <= 2020 && item.fuelId === "TO") {
+    if (item.period >= startYear && item.period <= endYear) {
       amount.push(item.value);
       year.push(item.period);
       sortYear = year.sort();
+      sectorArray.push(item.sectorId);
     }
   });
 
+  function evaluateOutput(sectorArray, amount, yearArr) {
+    const sectIdNames = ["CC", "IC", "TC", "EC", "RC"];
+
+    let len = sectorArray.length;
+    let findLen = [];
+
+    const R = sectIdNames.length;
+    const C = yearArr;
+    const val = 0;
+
+    let arr = [];
+
+    for (let i = 0; i < R; i++) {
+      arr[i] = [];
+
+      for (let j = 0; j < C; j++) {
+        arr[i][j] = val;
+        findLen[i] = 0;
+      }
+    }
+
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < R; j++) {
+        if (sectIdNames[j] === sectorArray[i]) {
+          arr[j][findLen[j]] = amount[i];
+          findLen[j] = findLen[j] + 1;
+        }
+      }
+    }
+
+    return arr;
+  }
+
+  let callFunc = evaluateOutput(sectorArray, amount, yearArr);
+
+  const sectorNames = [
+    "Commercial carbon dioxide emissions",
+    "Industrial carbon dioxide emissions",
+    "Transportation carbon dioxide emissions",
+    "Electric Power carbon dioxide emissions",
+    "Residential carbon dioxide emissions"
+  ];
+
+  //function to map sector id
+  function uniqueYearFunc(sortYear) {
+    const newArr = [];
+
+    let k = 0;
+    let len = sortYear.length;
+    for (let i = 0; i < len; i++) {
+      let temp = 0;
+      for (let j = 0; j < k; j++) {
+        if (sortYear[i] === newArr[j]) {
+          temp = 1;
+        }
+      }
+      if (temp === 0) {
+        newArr[k] = sortYear[i];
+        k = k + 1;
+      }
+    }
+
+    return newArr;
+  }
+
+  let newVariable = uniqueYearFunc(sortYear);
+
   const data = {
-    labels: sortYear,
+    labels: newVariable,
     datasets: [
       {
-        label: "CO2 Emissions",
-        data: amount,
-        backgroundColor: backgroundColors,
-        borderColor: borderColors,
+        label: sectorNames[0],
+        data: callFunc[0],
+        backgroundColor: "green",
+        borderColor: "green",
+        borderWidth: 1
+      },
+      {
+        label: sectorNames[1],
+        data: callFunc[1],
+        backgroundColor: "lightskyblue",
+        borderColor: "lightskyblue",
+        borderWidth: 1
+      },
+      {
+        label: sectorNames[2],
+        data: callFunc[2],
+        backgroundColor: "orange",
+        borderColor: "orange",
+        borderWidth: 1
+      },
+      {
+        label: sectorNames[3],
+        data: callFunc[3],
+        backgroundColor: "plum",
+        borderColor: "plum",
+        borderWidth: 1
+      },
+      {
+        label: sectorNames[4],
+        data: callFunc[4],
+        backgroundColor: "mediumorchid",
+        borderColor: "mediumorchid",
         borderWidth: 1
       }
-      // ,
-      // {
-      //   label: "dataset2",
-      //   data: amount,
-      //   backgroundColor: "rgba(255, 159, 64, 0.2)",
-      //   borderColor: "rgb(255, 159, 64)",
-      //   borderWidth: 1
-      // },
-      // {
-      //   label: "dataset3",
-      //   data: amount,
-      //   backgroundColor: "rgba(255, 205, 86, 0.2)",
-      //   borderColor: "rgb(255, 205, 86)",
-      //   borderWidth: 1
-      // },
-      // {
-      //   label: "dataset4",
-      //   data: amount,
-      //   backgroundColor: "rgba(75, 192, 192, 0.2)",
-      //   borderColor: "rgb(75, 192, 192)",
-      //   borderWidth: 1
-      // },
-      // {
-      //   label: "dataset5",
-      //   data: amount,
-      //   backgroundColor: "rgba(54, 162, 235, 0.2)",
-      //   borderColor: "rgb(54, 162, 235)",
-      //   borderWidth: 1
-      // },
-      // {
-      //   label: "dataset6",
-      //   data: amount,
-      //   backgroundColor: "rgba(153, 102, 255, 0.2)",
-      //   borderColor: "rgb(153, 102, 255)",
-      //   borderWidth: 1
-      // },
-      // {
-      //   label: "dataset7",
-      //   data: amount,
-      //   backgroundColor: "rgba(201, 203, 207, 0.2)",
-      //   borderColor: "rgb(201, 203, 207)",
-      //   borderWidth: 1
-      // }
     ]
   };
 
@@ -184,9 +159,9 @@ const BarChart = () => {
     <div className="barchart">
       <div className={`card bg-${mode.bg} text-${mode.text}`}>
         <div className="card-body">
-          <div className="card-title">
-            <h3>Sectorwise CO2 emissions for the most recent 5 years</h3>
-            <Bar data={data} height={400} />
+          <div className="card-title text-center">
+            <h5>Sector wise carbon emissions in Oregon over recent 5 years</h5>
+            <Bar data={data} options={{ aspectRatio: 1.6 }} />
           </div>
         </div>
       </div>
