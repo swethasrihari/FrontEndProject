80f00eb50e40e01cb5c3d877609e641d37bd4a63
Swetha Srihari <130093198+swethasrihari@users.noreply.github.com>
2023-06-09 19:59:56 -0700

    Updated Mode.js to delete commented statements

--- src/Utilities/Mode.js
+++ src/Utilities/Mode.js
@@ -2,36 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 function Mode(props) {
-  // const [theme, setTheme] = useState("light");
-  // const [textTheme, setTextTheme] = useState("dark");
-  // const toggleTheme = () => {
-  //   if (theme === "light") {
-  //     setTheme("dark");
-  //     setTextTheme("light");
-  //   } else {
-  //     setTextTheme("dark");
-  //     setTheme("light");
-  //   }
-  // };
-  // console.log("bg = " + theme);
-  // console.log("text = " + textTheme);
-  // useEffect(() => {
-  //   if (theme === "dark")
-  //     document.getElementsByClassName("App")[0].style.bachgroundImage = "";
-  //   else
-  //     document.getElementsByClassName("App")[0].style.bachgroundImage =
-  //       'Url("background.jpeg")';
-  //   document.getElementsByClassName(
-  //     "App"
-  //   )[0].className = `App bg-${theme} text-${textTheme}`;
-
-  //   for (let i = 0; i < document.getElementsByClassName("card").length; i++) {
-  //     document.getElementsByClassName("card")[
-  //       i
-  //     ].className = `card bg-${theme} text-${textTheme} shadow my-2 mx-2`;
-  //   }
-  // }, [theme, textTheme]);
-
   return (
     <div className={`Mode`}>
       <button
